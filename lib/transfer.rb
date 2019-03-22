503bf02e55f7fe4975ab8fefa536df842afd6aa3
Tracy Ngo <tngo1206@gmail.com>
2019-03-22 02:35:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   end
   
   def execute_transaction
-    if status != "complete" && self.valid? == true
+    if status != "complete" && self.valid?
       sender.balance -= amount
       receiver.balance += amount
       self.status = 'complete'
