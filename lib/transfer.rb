1ff73a060c887ac7546f4196d3b77f70d386ca92
Tracy Ngo <tngo1206@gmail.com>
2019-03-22 02:04:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,21 @@
 class Transfer
   # your code here
+  attr_accessor :amount
+  attr_reader :sender, :receiver,:status
+  
+  def initialize(sender, receiver, amount)
+    @amount = amount
+    @sender = sender
+    @receiver = receiver
+    @status = 'pending'
+  end
+  
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+  
+  def execute_transaction
+  
+  end
+  
 end
