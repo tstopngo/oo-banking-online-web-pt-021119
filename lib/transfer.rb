e71aec0f457d0edd185d8569ed36e84a5a66ddd9
Tracy Ngo <tngo1206@gmail.com>
2019-03-22 02:11:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
   
   def execute_transaction
-    if @status == "complete" || self.valid?
+    if @status == "complete" || !(self.valid?)
       "Transaction rejected. Please check your account balance."
     else
       sender.balance -= amount
